2a9c8bf361dabef3f0baf943e3f2844f0dd7f81c
PratimaP23 <156713745+PratimaP23@users.noreply.github.com>
2024-10-18 23:57:24 +0530

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -228,16 +228,60 @@ public static class Node{
 		}
 		 return true;
 	}
+	
+	public static boolean isCycle() {  //floyd's CFA
+		Node slow=head;
+		Node fast=head;
+		
+		while(fast!=null && fast.next!=null) {
+			slow=slow.next; //+1
+			fast=fast.next.next; //+2
+			if(slow==fast) {
+				return true; //cycle exists 
+			}
+		}
+		return false; //cycle does't exist
+	}
+	
+	public static void removeCycle() {
+		//detect cycle
+		Node slow=head;
+		Node fast=head;
+		boolean cycle = false;
+		while(fast!=null &&  fast.next!=null) {
+			slow=slow.next;
+			fast=fast.next.next;
+			if(fast==slow) {
+				cycle=true;
+				break;
+			}
+		}
+		if(cycle==false) {
+			return;
+		}
+		
+		//find meeting point
+		slow=head;
+		Node prev=null;
+		while(slow!=fast) {
+			prev=fast;
+			slow=slow.next;
+			fast=fast.next;
+		}
+		
+		//remove cycle-> last.next=null
+		prev.next=null;
+	}
 
 	public static void main(String[] args) {
-		
-		LinkedList ll =new LinkedList();
-		ll.addLast(1);
-		ll.addLast(2);
-		ll.addLast(1);
-		//ll.addLast(1);
-		
-		ll.print(); //1->2->2 
-		System.out.println(ll.checkPalindrome());
+		head=new Node(1);
+		Node temp=new Node(2);
+		head.next=temp;
+		head.next.next=new Node(3);
+		head.next.next.next=temp;
+		//1->2->3->2 
+		System.out.println(isCycle());
+		removeCycle();
+		System.out.println(isCycle());
 	}
 }
